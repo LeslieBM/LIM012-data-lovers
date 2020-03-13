431e8b6805a65b24de76b63c084f323cd961a32b
Your Name <judithportocarrero99@gmail.com>
2020-03-13 13:06:21 -0500

    judith p

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { example } from './data.js';
-// import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
-
 console.log(example, data);
+
+const recorrerArray = (pokemon, index) =>{
+    document.getElementById("demo").innerHTML += '<div id="contenedor"><div>'+pokemon.num + '</div><div>' + pokemon.name + '</div><br><img src="'+pokemon.img+'">'+'<p>'+pokemon.type+'</p></div>';
+} 
+data.pokemon.forEach(recorrerArray);
+
