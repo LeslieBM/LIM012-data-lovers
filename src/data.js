83df4a1ba646b950d0d9b3ce83ffb515daa292d2
unknown <judithportocarrero99@gmail.com>
2020-03-16 19:03:00 -0500

    judith

--- src/data.js
+++ src/data.js
@@ -1,5 +1,23 @@
 // estas funciones son de ejemplo
-
-export const example = () => 'example';
-
-export const anotherExample = () => 'OMG';
+export const sortNumAsc = (pokemon) => {
+  document.getElementById('demo').innerHTML += `
+    <div class="contenedor">
+      <img src="${pokemon.img}">
+      <p>N°${pokemon.num}</p>
+      <p>${pokemon.name}</p>
+      <p>${pokemon.type}</p>
+    </div>`;
+};
+export const sortNumDesc = (pokemon) => {
+  let arrPokemonesNumDesc = [];
+  arrPokemonesNumDesc = pokemon.sort((a, b) => {
+    return b.num - a.num;
+  });
+  document.getElementById('demo1').innerHTML += `
+    <div class="contenedor">
+      <img src="${arrPokemonesNumDesc.img}">
+      <p>N°${arrPokemonesNumDesc.num}</p>
+      <p>${arrPokemonesNumDesc.name}</p>
+      <p>${arrPokemonesNumDesc.type}</p>
+    </div>`;
+};
--- src/main.js
+++ src/main.js
@@ -1,30 +1,68 @@
-import { example } from './data.js';
+// import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
-console.log(example, data);
-let  sortNumDesc = ( pokemon ) =>{
-    console.log(pokemon.forEach(pokemon.sort((a,b) => b.num - a.num)));
-};
+const pokemon = data.pokemon;
 
-// const mayorMenor = (pokemon, index) =>{
-//     let nuevoArray = pokemon.sort(function (a, b){
-//         if(b.num - a.num))return (b.num - a.num);
-//     })
-//     document.getElementById("demo").classList.add("ocultar");
-//     document.getElementById("demo1").classList.remove("ocultar");
-//     document.getElementById("demo1").innerHTML += '<div class="contenedor"><div>'+pokemon.num + '</div><div>' + pokemon.name + '</div><br><img src="'+pokemon.img+'">'+'<p>'+pokemon.type+'</p></div>';
+const recorrerArray = (pokemon) => {
+  document.getElementById('demo').innerHTML += `
+  <div class="contenedor">
+    <img src="${pokemon.img}">
+    <p>N°${pokemon.num}</p>
+    <p>${pokemon.name}</p>
+    <p>${pokemon.type}</p>
+  </div>`;
+  const sortNumAsc = (pkm) => {
+    console.log('3.0');
+    let arrPokemonesNumDesc = [];
+    arrPokemonesNumDesc = pkm.sort((a, b) => {
+      return a.num - b.num;
+    });
+    return arrPokemonesNumDesc;
+  };
+  console.log('1');
+};
+const reccorer = () => {
+  document.getElementById('demo1').classList.add('ocultar');
+  document.getElementById('demo').classList.remove('ocultar');
+  data.pokemon.forEach(recorrerArray);
+  console.log('2');
+};
+document.getElementById('men').addEventListener('click', reccorer);// menor a mayor
+// mayor a menor
+// const sortNumAsc = (pkm) => {
+//   console.log('3.0');
+//   let arrPokemonesNumDesc = [];
+//   arrPokemonesNumDesc = pkm.sort((a, b) => {
+//     return a.num - b.num;
+//   });
+//   return arrPokemonesNumDesc;
 // };
-document.getElementById("may").addEventListener("click", sortNumDesc);//mayor a menor
-
-
-const recorrerArray = (pokemon, index) =>{
-    document.getElementById("demo").innerHTML += '<div class="contenedor"><div>'+pokemon.num + '</div><div>' + pokemon.name + '</div><br><img src="'+pokemon.img+'">'+'<p>'+pokemon.type+'</p></div>';
+const sortNumDesc = (pkm) => {
+  console.log('3');
+  let arrPokemonesNumDesc = [];
+  arrPokemonesNumDesc = pkm.sort((a, b) => {
+    return b.num - a.num;
+  });
+  return arrPokemonesNumDesc;
 };
-let reccorer = () =>{
-    document.getElementById("demo1").classList.add("ocultar");
-    document.getElementById("demo").classList.remove("ocultar");
-    data.pokemon.forEach(recorrerArray);
+const recorrerArray2 = (pkm) => {
+  document.getElementById('demo1').innerHTML += `
+  <div class="contenedor">
+    <img src="${pkm.img}">
+    <p>N°${pkm.num}</p>
+    <p>${pkm.name}</p>
+    <p>${pkm.type}</p>
+  </div>`;
+  console.log('4');
 };
-document.getElementById("men").addEventListener("click", reccorer);//menor a mayor
-  
-document.getElementById("az");
-document.getElementById("za");
+const reccorer2 = () => {
+  document.getElementById('demo').classList.add('ocultar');
+  document.getElementById('demo1').classList.remove('ocultar');
+  pokemon.forEach(sortNumDesc(pokemon));
+  console.log('5');
+};
+// document.getElementById("demo").classList.add("ocultar");
+// document.getElementById("demo1").classList.remove("ocultar");
+// document.getElementById("demo1").innerHTML += '<div class="contenedor"><div>'+pokemon.num + '</div><div>' + pokemon.name + '</div><br><img src="'+pokemon.img+'">'+'<p>'+pokemon.type+'</p></div>';
+document.getElementById('may').addEventListener('click', sortNumDesc(pokemon)); // mayor a menor
+document.getElementById('az');
+document.getElementById('za');
