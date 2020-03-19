8c70ae8eea466fbf61538491ccda2e792f094eb4
LeslieBM <lesliebenelmendoza@gmail.com>
2020-03-19 12:16:40 -0500

    Cambios

--- src/data.js
+++ src/data.js
@@ -6,35 +6,6 @@
 
 // };
 export const ordenarArrayAsc = (arr) => {
-<<<<<<< HEAD
-    let arrPokemonesNumAsc = [];
-    arrPokemonesNumAsc = arr.sort((a, b) => a.num - b.num);
-    return arrPokemonesNumAsc;
-  };
-  export const ordenarArrayDes = (arr) => {
-    let arrPokemonesNumDesc = [];
-    arrPokemonesNumDesc = arr.sort((a, b) => b.num - a.num);
-    return arrPokemonesNumDesc;
-  };
-  export const ordenarArrayAz = (arr) => {
-    let arrPokemonesAz = [];
-    arrPokemonesAz = arr.sort((a, b) => {
-      if (a.name > b.name) {
-        return 1;
-      } return -1;
-    });
-    return arrPokemonesAz;
-  };
-  export const ordenarArrayZa = (arr) => {
-    let arrPokemonesZa = [];
-    arrPokemonesZa = arr.sort((a, b) => {
-      if (b.name > a.name) {
-        return 1;
-      } return -1;
-    });
-    return arrPokemonesZa;
-  };
-=======
   let arrPokemonesNumAsc = [];
   arrPokemonesNumAsc = arr.sort((a, b) => a.num - b.num);
   return arrPokemonesNumAsc;
@@ -62,4 +33,3 @@ export const ordenarArrayZa = (arr) => {
   });
   return arrPokemonesZa;
 };
->>>>>>> d2b0644835884785891f70c17df963f884f15c42
