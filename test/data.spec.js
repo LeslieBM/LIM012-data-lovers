5f0ca79d82240cd1d1e91e0a2036caaa3f77a80c
unknown <judithportocarrero99@gmail.com>
2020-03-22 14:26:04 -0500

    22/03

--- test/data.spec.js
+++ test/data.spec.js
@@ -2,26 +2,30 @@ import {
   ordenarArrayAsc, ordenarArrayDes, ordenarArrayAz, ordenarArrayZa,
 } from '../src/data.js';
 
-describe('ordenarArrayAsc', () => {
+describe('ordenado Ascendente', () => {
   it('is a function', () => {
     expect(typeof ordenarArrayAsc).toBe('function');
   });
+  test('ordenar', () => {
+    expect(ordenarArrayAsc()).toBe();
+  });
 });
-describe('ordenarArrayDes', () => {
+describe('ordenado Descendente', () => {
   it('is a function', () => {
     expect(typeof ordenarArrayDes).toBe('function');
   });
 });
-describe('ordenarArrayAz', () => {
+describe('ordenado de Az', () => {
   it('is a function', () => {
     expect(typeof ordenarArrayAz).toBe('function');
   });
 });
-describe('ordenarArrayZa', () => {
+describe('ordenado de Za', () => {
   it('is a function', () => {
     expect(typeof ordenarArrayZa).toBe('function');
   });
 });
+//---------------------------------------
 // import { example, anotherExample } from '../src/data.js';
 // describe('example', () => {
 //   it('is a function', () => {
