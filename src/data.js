0e07a14ea01c49e4975b69224d05d619e780b364
LeslieBM <lesliebenelmendoza@gmail.com>
2020-04-02 01:35:23 -0500

    Avance Proyecto Pokemon

--- src/data.js
+++ src/data.js
@@ -24,6 +24,7 @@ export const sortArray = (arr, condition) => {
     });
   }
   return arrPokemon;
+<<<<<<< HEAD
 };
   // filtrar por km
 export const filterKm = (arr, condition) => {
@@ -58,11 +59,51 @@ export const compareOnePokemon = (arr, condition) => {
   const arrFilterName = arr.filter(object => object.name === condition);
   return arrFilterName;
 };
+=======
+};
+// filtrar por km
+export const filterKm = (arr, condition) => {
+  let arrFilterKM = [];
+  if (condition === 'oneKm') {
+    arrFilterKM = arr.filter(object => object['buddy-distance-km'] === '1');
+  } else if (condition === 'threeKm') {
+    arrFilterKM = arr.filter(object => object['buddy-distance-km'] === '3');
+  } else if (condition === 'fiveKm') {
+    arrFilterKM = arr.filter(object => object['buddy-distance-km'] === '5');
+  } else if (condition === 'twentyKm') {
+    arrFilterKM = arr.filter(object => object['buddy-distance-km'] === '20');
+  }
+  return arrFilterKM;
+};
+export const sortPower = (arr, condition) => {
+  let arrPokemonPower = [];
+  if (condition === 'atack') {
+    arrPokemonPower = arr.sort((a, b) => b.stats['base-attack'] - a.stats['base-attack']);
+  } else if (condition === 'defense') {
+    arrPokemonPower = arr.sort((a, b) => b.stats['base-defense'] - a.stats['base-defense']);
+  } else if (condition === 'health') {
+    arrPokemonPower = arr.sort((a, b) => b.stats['base-stamina'] - a.stats['base-stamina']);
+  } else if (condition === 'maxCp') {
+    arrPokemonPower = arr.sort((a, b) => b.stats['max-cp'] - a.stats['max-cp']);
+  } else {
+    arrPokemonPower = arr.sort((a, b) => b.stats['max-hp'] - a.stats['max-hp']);
+  }
+  return arrPokemonPower;
+};
+export const compareOnePokemon = (arr, condition) => {
+  const arrFilterName = arr.filter(object => object.name === condition);
+  return arrFilterName;
+};
+>>>>>>> 595f9e3... Proyecto Pokemon Go
 export const compareTwoPokemon = (arr, condition) => {
   const arrFilterName = arr.filter(object => object.name === condition);
   return arrFilterName;
 };
+<<<<<<< HEAD
   // buscador
+=======
+// buscador
+>>>>>>> 595f9e3... Proyecto Pokemon Go
 export const searchPokemon = (arr, condition) => {
   const pokemon = arr.filter(object => (object.name).indexOf(condition) === 0);
   return pokemon;
